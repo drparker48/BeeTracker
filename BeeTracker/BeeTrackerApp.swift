05ff3d33cb7bc5050e26a09f7d9017d599d54f71
drparker48 <davidrobertparker@icloud.com>
2024-03-28 17:25:01 +0000

    Initial Commit

---
+++ BeeTracker/BeeTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeeTrackerApp.swift
+//  BeeTracker
+//
+//  Created by David Parker on 28/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct BeeTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
